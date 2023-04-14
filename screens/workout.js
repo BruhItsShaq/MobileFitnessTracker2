8906671dfa3cb154eec55a42a04fcb07e258ecfd
SHAQS-LAPTOP\shaqe <shaqeil567@outlook.com>
2023-04-14 01:24:19 +0100

    Screens created for rest of app

---
+++ screens/workout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const WorkoutScreen = () => {
+    return (
+        <View>
+            <Text>Workout Screen</Text>
+        </View>
+    );
+}
+
+export default WorkoutScreen;
